edd9f2a6752be6070f4ad3f021c2c42677b4c4ef
Courtney Yoo <c.yoo@wustl.edu>
2018-06-06 20:09:02 -0500

    write Restaurant codable

--- EatMyWayAround/Restaurant.swift
+++ EatMyWayAround/Restaurant.swift
@@ -1,5 +1,18 @@
 import Foundation
 
 class Restaurant: Codable {
-    
+    var name: String
+    var location: LocationDetails
+    var cuisines: String
+    var average_cost_for_two: Int
+    var user_rating: Ratings
+}
+
+class LocationDetails: Codable {
+    var address: String
+}
+
+class Ratings: Codable {
+    var aggregate_rating: Double
+    var votes: Int
 }
